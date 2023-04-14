f77c35417ebfd0ac95a01e47ae24531476787832
Mithil Dani <danimithil@gmail.com>
2023-04-13 23:37:52 -0400

    added payment creation

--- src/wolfmedia/api/PaymentDao.java
+++ src/wolfmedia/api/PaymentDao.java
@@ -77,7 +77,8 @@ public class PaymentDao {
         try {
             Connection conn = DBConnection.getConnection();
             PreparedStatement stmt = conn.prepareStatement(
-                    "SELECT ArtistID, SUM(Amount*0.7/NOA.NumberOfArtists) as ArtistPayment, EP.Year as Year, EP.Month as Month" +
+                    "SELECT ArtistID, SUM(Amount*0.7/NOA.NumberOfArtists) as ArtistPayment, EP.Year as Year, EP.Month as Month"
+                            +
                             "FROM ExpensePayment EP" +
                             "INNER JOIN (" +
                             "  SELECT DISTINCT S.MediaID, ? as ArtistID" +
@@ -124,6 +125,88 @@ public class PaymentDao {
             return rs;
         } catch (SQLException e) {
             System.out.println("Error retrieving record label payment: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    public boolean createSongPayment(int mediaId, int year, int month) throws SQLException {
+        String sql = "INSERT INTO ExpensePayment (MediaID, Year, Month, Amount, CreatedAt)" +
+                "VALUE (?, ?, ?," +
+                "                    (" +
+                "                        SELECT SUM(Count) as 'TotalPlayCount'" +
+                "                        FROM Playbacks" +
+                "                        WHERE MediaID= ? " +
+                "                        AND YEAR(PlayedAt) = ? " +
+                "                        AND MONTH(PlayedAt) = ? " +
+                "                        GROUP BY MediaID" +
+                "                    ) *" +
+                "                    (" +
+                "                        SELECT RoyaltyRate" +
+                "                        FROM Song" +
+                "                        WHERE MediaID= ? " +
+                "                    ), DATE(NOW())" +
+                "        )";
+        Connection conn = DBConnection.getConnection();
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, mediaId);
+            stmt.setInt(2, year);
+            stmt.setInt(3, month);
+            stmt.setInt(4, mediaId);
+            stmt.setInt(5, year);
+            stmt.setInt(6, month);
+            stmt.setInt(7, mediaId);
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error Creating Payment");
+            throw e;
+        }
+    }
+
+    public boolean createPodcastPayment(int hostId, int year, int month) throws SQLException {
+        String podSql = "INSERT INTO ExpensePayment (MediaID, Year, Month, Amount, CreatedAt)" +
+                "(SELECT PE.MediaID," +
+                "          YEAR(AP.PlayedAt) as Year," +
+                "          MONTH(AP.PlayedAt) as Month," +
+                "          FlatFee+AdRate*SUM(Count) as Amount," +
+                "          DATE(NOW()) as CreatedAt" +
+                "   FROM PodcastEpisode PE" +
+                "   INNER JOIN Podcast P on PE.PodcastID = P.PodcastID" +
+                "   INNER JOIN PodcastHost PH on P.HostID = PH.HostID" +
+                "   INNER JOIN AdsPlayed AP on PE.MediaID = AP.MediaID" +
+                "   WHERE P.HostID= ? " +
+                "   AND YEAR(AP.PlayedAt) = ?" +
+                "   AND MONTH(AP.PlayedAt) = ?" +
+                "   GROUP BY PE.MediaID, Year, Month)";
+        Connection conn = DBConnection.getConnection();
+        try (PreparedStatement stmt = conn.prepareStatement(podSql)) {
+            stmt.setInt(1, hostId);
+            stmt.setInt(2, year);
+            stmt.setInt(3, month);
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error Creating Payment");
+            throw e;
+        }
+    }
+
+    public boolean createSubscriberPayment(int userId, int year, int month) throws SQLException {
+        String sql = "INSERT INTO RevenuePayment (UserID, Month, Year, Amount, PaymentDate)" +
+                "(" +
+                "   SELECT UserID, ?, ?, Fee, DATE(NOW())" +
+                "   FROM Users" +
+                "   WHERE Users.UserID = ?" +
+                ")";
+        Connection conn = DBConnection.getConnection();
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            stmt.setInt(3, userId);
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error Creating Payment");
             throw e;
         }
     }
